21aa5c290a9f1d15a69cd94b8550d7c15c36bed4
mmleziva <marek.mleziva@quick.cz>
2026-08-19 17:53:06 +0200

    Initial commit - poolroof motor controller

---
+++ Core/Inc/motor.h
@@ -0,0 +1,30 @@
+/*
+ * motor.h
+ *
+ *  Created on: 10. 8. 2026
+ *      Author: DELL
+ */
+
+#ifndef INC_MOTOR_H_
+#define INC_MOTOR_H_
+
+#include "stm32g4xx_hal.h"
+#include "main.h"
+
+uint8_t GetSTOP(void);
+uint8_t GetOTV(void);
+uint8_t GetINKREM(void);
+
+uint8_t GetKON_Z(void);
+uint8_t GetKON_O(void);
+uint8_t GetZAV(void);
+typedef enum
+{
+    MOTOR_STOP = 0,
+    MOTOR_FORWARD,
+    MOTOR_REVERSE
+} MotorDirection;
+
+void Motor_SetSpeed(MotorDirection dir, uint16_t speed);
+
+#endif /* INC_MOTOR_H_ */
